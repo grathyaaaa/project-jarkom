8a3da86e19d890d0687174d080982882763c0c78
grathyaaaa <grathya.thiya@gmail.com>
2019-11-28 21:07:17 +0800

    selesai

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ import requests
 r = requests.get(URL, headers=HEADERS)
 html = r.text
 soup = BeautifulSoup(html, features='lxml')
-data = soup.find('table')
+data = soup.findAll('td')
+datas = []
+for i in range(len(data)//7):
+    sub_data=[]
+    #sub_data.append(data[i*7+0].text)
+    #sub_data.append((data[i*7+1].text).split(",")[0])
+    #sub_data.append((data[i*7+2].text).split(",")[0])
+    sub_data.append((data[i*7+3].text).split(",")[0])
+    sub_data.append((data[i*7+4].text).split(",")[0])
+    sub_data.append((data[i*7+5].text).split(",")[0])
+    sub_data.append((data[i*7+6].text).split(",")[0])
+    datas.append(sub_data)
 ###
+datas = list(map(list, zip(*datas)))
 
-
+for i in range(len(datas)):
+    for j in range(len(datas[0])):
+        datas[i][j] = datas[i][j].replace(".",'')
 
 ### 
 from flask import Flask, Blueprint, render_template
@@ -20,7 +34,7 @@ web_app = Blueprint('app', __name__)
 
 @web_app.route('/')
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=datas)
 
 if __name__ == '__main__':
     app = Flask(__name__)
